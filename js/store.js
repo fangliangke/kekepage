6c8461ebc394bfcfa8a9c2f29e0a0492dc86a7b6
fangliangke <1254818550@qq.com>
2022-12-13 13:52:24 +0800

    mybatis-tool

--- js/store.js
+++ js/store.js
@@ -1,6 +1,6 @@
 const store = new Vuex.Store({
     state: {
-        typeList: ['综合', '有趣', '仿照', '游戏'],
+        typeList: ['综合', '有趣', '仿照', '游戏', '工具'],
         typeIndex: 0,
         articleList: [{
             type: '仿照',
@@ -21,6 +21,16 @@ const store = new Vuex.Store({
                 like: 99,
                 cover: 'cat.jpeg',
                 url: 'funny/christmasTree.html'
+            },
+            {
+                type: '工具',
+                createTime: '2022年12月13日',
+                title: 'mybatis日志解析成可执行SQL',
+                content: '一个快速mybatis日志解析成可执行SQL工具',
+                view: 100,
+                like: 99,
+                cover: 'cat.jpeg',
+                url: 'tool/mybatis-tool.html'
             }]
     },
     getters: {
@@ -28,13 +38,13 @@ const store = new Vuex.Store({
             if (state.typeIndex === 0) {
                 return state.articleList;
             } else {
-                return state.articleList.filter((obj)=> state.typeList[state.typeIndex] === obj.type)
+                return state.articleList.filter((obj) => state.typeList[state.typeIndex] === obj.type)
             }
         }
     },
     mutations: {
-        setTypeIndex(state,value) {
-          state.typeIndex = value;
+        setTypeIndex(state, value) {
+            state.typeIndex = value;
         }
     },
     actions: {
