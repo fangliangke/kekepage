95df43d866aa00dabea2361119b0fe7916866e25
fangliangke <1254818550@qq.com>
2023-01-06 16:11:16 +0800

    字符串切割

--- js/store.js
+++ js/store.js
@@ -2,6 +2,8 @@ const store = new Vuex.Store({
     state: {
         typeList: ['综合', '有趣', '仿照', '游戏', '工具'],
         typeIndex: 0,
+        //关键字搜索
+        searchText:'',
         articleList: [{
             type: '仿照',
             createTime: '2022年11月11日',
@@ -31,25 +33,58 @@ const store = new Vuex.Store({
                 like: 99,
                 cover: 'cat.jpeg',
                 url: 'tool/mybatis-tool.html'
-            }]
+            },
+            {
+                type: '工具',
+                createTime: '2023年1月03日',
+                title: '字符串切割',
+                content: '可以将excel中一行数据切割为数组方便sql作in操作',
+                view: 100,
+                like: 99,
+                cover: 'cat.jpeg',
+                url: 'tool/string-tool.html'
+            }
+            ]
     },
     getters: {
         getArticleList(state) {
+            debugger;
             if (state.typeIndex === 0) {
-                return state.articleList;
+                return state.articleList.filter(obj => {
+                    if (state.searchText.length >0) {
+                        return obj.title.includes(state.searchText)
+                    }else{
+                        return true;
+                    }
+                });
             } else {
-                return state.articleList.filter((obj) => state.typeList[state.typeIndex] === obj.type)
+                return state.articleList
+                    .filter((obj) => {state.typeList[state.typeIndex] === obj.type})
+                    .filter(obj => {
+                        if (state.searchText.length >0) {
+                            return obj.title.includes(state.searchText)
+                        }else{
+                            return true
+                        }
+                    });
             }
         }
     },
     mutations: {
         setTypeIndex(state, value) {
             state.typeIndex = value;
-        }
+        },
+        // 搜索值
+        setSearchText(state, value) {
+            state.searchText = value
+        },
     },
     actions: {
         setTypeIndex: ({
                            commit
                        }, value) => commit('setTypeIndex', value),
+        setSearchText: ({
+                           commit
+                       }, value) => commit('setSearchText', value),
     }
 })
